bd103a2fbd02541a1abdc119fe3be1673f43bf86
kkonat <presto@presto.biz.pl>
2023-04-24 23:01:53 +0200

    add err handling

--- README.md
+++ README.md
@@ -6,6 +6,12 @@ A simple app to download ICM meteogram to my phone
 run:
 flutter run -d edge --web-renderer html
 
+generate launcher icons:
+flutter pub run flutter_launcher_icons
+https://pub.dev/packages/flutter_launcher_icons
 
 build:
 flutter build web --web-renderer html --release
+
+build for my android:
+flutter run -d "VIE L09" --release
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,102 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
+import 'package:http/http.dart' as http;
 
 const appName = "Meteo";
+
+// coordinates for Warsaw
+// check yours here: https://m.meteo.pl/
+class Loc {
+  static double x = 250.0, y = 410.0;
+}
+
 void main() {
   runApp(const App());
 }
 
 class App extends StatelessWidget {
   const App({super.key});
+  get mapUrl => 'https://m.meteo.pl/img/um_oreografia.png';
   @override
   build(context) => MaterialApp(
       title: appName,
-      home: Scaffold(
-          appBar: AppBar(title: Text("$appName for $datetime")),
-          body: Center(
-              child: InteractiveViewer(
-            minScale: 1.0,
-            maxScale: 3.0,
-            child: Image.network(url),
-          ))));
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+            appBar: AppBar(
+                title: Text("$appName for $datetime"),
+                bottom: const TabBar(tabs: [
+                  Tab(text: "Meteo"),
+                  Tab(text: "Location"),
+                ])),
+            body: TabBarView(
+              children: [
+                Center(
+                    child: InteractiveViewer(
+                  minScale: 1.0,
+                  maxScale: 3.0,
+                  child: getMeteogram(),
+                )),
+                Center(
+                    child: InteractiveViewer(
+                  onInteractionUpdate: (v) {
+                    Loc.x = v.localFocalPoint.dx;
+                    Loc.y = v.localFocalPoint.dy;
+                    // print(currentScale);
+                    print(v.localFocalPoint.dx);
+                    print(v.localFocalPoint.dy);
+                  },
+                  minScale: 1.0,
+                  maxScale: 3.0,
+                  child: Stack(children: [
+                    getImage(mapUrl, BoxFit.none),
+                    Positioned(
+                      left: Loc.x.toDouble(),
+                      top: Loc.y.toDouble(),
+                      child: Container(
+                        width: 5,
+                        height: 5,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.red,
+                        ),
+                      ),
+                    ),
+                  ]),
+                )),
+              ],
+            )),
+      ));
+
+  Image getMeteogram() {
+    const url =
+        'https://meteo.pl/um/metco/mgram_pict.php?ntype=0u&fdate=$datetime&col=${Loc.x}&row=${Loc.y}&lang=en';
+
+    return getImage(url, BoxFit.contain);
+  }
+
+  Image getImage(imgurl, fit) {
+    var NI = NetworkImage(imgurl);
+    // check if image has loaded
+    NI.resolve(const ImageConfiguration()).addListener(
+          ImageStreamListener(
+            (info, call) {
+              print('NetworkImage loaded: $imgurl');
+            },
+            onError: (exception, stackTrace) {
+              print('NetworkImage failed to load: $imgurl');
+              // print('$exception\n$stackTrace');
+            },
+          ),
+        );
+
+    return Image(
+      fit: fit,
+      // width: window.physicalSize.width,
+      // height: window.physicalSize.height,
+      image: NI,
+    );
+  }
 
   get datetime {
     var now = DateTime.now().subtract(const Duration(hours: 6));
@@ -26,7 +105,4 @@ class App extends StatelessWidget {
         now.day * 100 +
         now.hour ~/ 6 * 6;
   }
-
-  get url =>
-      'https://meteo.pl/um/metco/mgram_pict.php?ntype=0u&fdate=$datetime&row=409&col=248&lang=en';
 }
